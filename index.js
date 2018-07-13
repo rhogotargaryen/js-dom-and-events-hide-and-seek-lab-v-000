652c6ed42f243ac6049a62af9bd21e853c80178a
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-13 23:19:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node
+}
+
+function increaseRankBy(n) {
+    const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+    for (let i = 0; i < lis.length; i++) {
+     let a = parseInt(lis[i].innerHTML);
+     lis[i].innerHTML = (a + n).toString();
+    }
+}
